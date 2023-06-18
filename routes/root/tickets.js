84e925dc99ab71f0e8af945878abf334865f524a
Breno <brenosc2002@edu.unifor.br>
2023-06-18 19:13:30 -0300

    Eu que fiz

--- routes/root/tickets.js
+++ routes/root/tickets.js
@@ -5,9 +5,10 @@ const router = Router()
 
 // Rota GET para obter todos os tickets
 router.get('/', async (request, response) => {
-  const { owner_id } = request.query
+  const owner_id = request.query.owner_id
 
   try {
+    //Se tiver um owner_id definido, entao encontre os tickets com ESSE owner_id. Se NÃO encontre TODOS
     const tickets = owner_id ? await Ticket.find({ owner_id }) : await Ticket.find()
     return response.status(200).json(tickets)
   } catch (err) {
@@ -21,9 +22,12 @@ router.get('/:id', async (request, response) => {
   const { id } = request.params
 
   try {
+    //Encontrar um ticket no banco de dados com o _id (id banco de dados) = id (id da url)
     const ticket = await Ticket.findById(id)
+
+    //  envia msg dde erro caso nenhum ticket com o id especificado seja encontrado
     if (!ticket) {
-      return response.sendStatus(404).json({ msg: 'Ticket not found' })
+      return response.status(404).json({ msg: 'Ticket not found' })
     }
     return response.status(200).json(ticket)
   } catch (err) {
@@ -33,12 +37,15 @@ router.get('/:id', async (request, response) => {
 })
 
 router.post('/', async (request, response) => {
+  // Estou querendo apenas esses 3 campos do objeto request.body
   const { owner_id, nome, valor } = request.body
 
+  // Se algum dos campos estiver auxente, ele acusa um erro
   if (!owner_id || !nome || !valor) {
-    return response.sendStatus(422).json({ msg: 'All fields must be filled' })
+    return response.status(422).json({ msg: 'All fields must be filled' })
   }
 
+  //Cria um objeto do modelo Ticket
   try {
     const ticket = new Ticket({
       owner_id,
@@ -59,12 +66,14 @@ router.post('/', async (request, response) => {
 // Rota PUT para atualizar um ticket
 router.put('/:id', async (request, response) => {
   const { id } = request.params
+  // Estou querendo apenas esses 3 campos do objeto request.body
   const { owner_id, nome, valor } = request.body
 
   try {
     const ticket = await Ticket.findById(id)
+    //  envia msg dde erro caso nenhum ticket com o id especificado seja encontrado
     if (!ticket) {
-      return response.sendStatus(404).json({ msg: 'Ticket not found' })
+      return response.status(404).json({ msg: 'Ticket not found' })
     }
 
     ticket.owner_id = owner_id
@@ -87,7 +96,7 @@ router.delete('/:id', async (request, response) => {
   try {
     const ticket = await Ticket.findById(id)
     if (!ticket) {
-      return response.sendStatus(404).json({ msg: 'Ticket not found' })
+      return response.status(404).json({ msg: 'Ticket not found' })
     }
 
     await ticket.deleteOne()
